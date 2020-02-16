ca57a2c5bd03ca35624c51af938ca27fab5e6d1d
Sara <sara.marlop87@gmail.com>
2020-02-16 16:19:09 +0100

    project_setup file

--- scripts/project_setup.R
+++ scripts/project_setup.R
@@ -13,14 +13,21 @@ setwd(dirname(current_path))
 setwd("..")
 rm(current_path)
 
-# Install some packages 
+# Install digest package 
+# "Do you want to reinitiate R? --> Yes)
+# "Do you want to install from sources the package which needs 
+# compilation?" --> Yes)
 install.packages("digest", dependencies=TRUE)
+
+# Install datastorr package (You can skip updates selecting the option 3:none)
+options(repos = getOption("repos")["CRAN"])
 devtools::install_github("ropenscilabs/datastorr")
 
 # Install the lemis package (download Github repo)
 # The lemis R package provides access to the United States Fish and Wildlife 
 # Service's (USFWS) Law Enforcement Management Information System (LEMIS) data 
 # on wildlife and wildlife product imports into the US.
+# (You can skip updates selecting the option 3:none)
 devtools::install_github("ecohealthalliance/lemis")
 
 # Reading dataset  - You will need to download the dataset the first time - 
